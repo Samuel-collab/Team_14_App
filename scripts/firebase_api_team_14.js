727b503eec9db2a51f3772a714af3b6af85b3b60
Samuel Cheon <samuelcalling@gmail.com>
2021-03-05 21:35:18 -0800

    minor format fixes to index.html and sort.html. Created login.html and firebase_api_team_14.js for user  login

---
+++ scripts/firebase_api_team_14.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDH81EQsgS3_PUOSQ4F7IQO8PmZ6sm8CEw",
+    authDomain: "comp1800team14.firebaseapp.com",
+    projectId: "comp1800team14",
+    storageBucket: "comp1800team14.appspot.com",
+    messagingSenderId: "470167894736",
+    appId: "1:470167894736:web:1e74209941b4e87564bcd5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+
