68af4984639cb0f687c61a0757187c68922c0cdd
kwanyong <lunaticjky@gmail.com>
2021-03-16 12:05:30 -0700

    add read and write db

---
+++ scripts/parkFacility.js
@@ -0,0 +1,27 @@
+var parkFacilitySet = db.collection("parkFacility").doc("facility");
+
+parkFacilitySet.set({
+    list: "Restaurant"
+
+}).then(function() {
+    console.log("Document is written!");
+}).catch(function(err) {
+    console.log(err);
+});
+
+parkFacilitySet.update({
+    createdAt: firebase.firestore.FieldValue.serverTimestamp()
+})
+
+function readBMPname() {
+    parkFacilitySet.get().then(function(doc) {
+        if (doc.exists) {
+            console.log("Document data: ", doc.data());
+            $(".parkFacility").text(doc.data().list);
+        } else {
+            console.log("Not found");
+        }
+    })
+
+}
+readBMPname();
