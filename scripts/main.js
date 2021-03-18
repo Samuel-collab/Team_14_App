1ca13434feb032b7f295fe2c4b779d70d15b5721
Samuel Cheon <samuelcalling@gmail.com>
2021-03-18 15:58:11 -0700

    modifed main.html to add a user greeting. Also created main.js

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+var docRef = db.doc('users/iEgnkNvQG2cY8MbluqI99JDJXdz2');
+
+docRef.get().then(function(doc) {
+    if (doc.exists) {
+        console.log("This users data: ", doc.data())
+        $(".userName").append(" ").append(doc.data().name);
+    } else {
+        console.log("No such data exists!");
+    }
+}).catch(function(error) {
+    console.log("Error getting document");
+});
