d195e06c06da63b81b5088a8c647562bc88b938f
Samuel Cheon <samuelcalling@gmail.com>
2021-04-02 13:54:28 -0700

    Changed main to add a greeting to user when logged in.

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,28 @@ docRef.get().then(function(doc) {
 }).catch(function(error) {
     console.log("Error getting document");
 }); */
+
+//*****************On user login or signup, mainpage will hold a greeting to user****************
+//First, find user ID.
+function sayHello() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            // Do something for the user here. 
+            console.log(user.uid);
+            db.collection("users").doc(user.uid)
+                .get()
+                .then(function (doc) {
+                    var n = doc.data().name;
+                    console.log(n);
+										//using jquery
+                    $("#greeting-to-user").text(n + ", you are successfully logged in!");
+										//using vanilla javascript
+                    //document.getElementById("username").innerText = n;
+                })
+        } else {
+            // No user is signed in.
+        }
+    });
+}
+sayHello();
